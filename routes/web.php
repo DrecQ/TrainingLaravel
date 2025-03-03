15cf4a0b37e8fb5107146621b58d9a85e7ba0b3a
DrecQ <drecquist@gmail.com>
2025-03-03 18:00:12 +0100

    Gestion des routes / Creation des models eloquent / Utilisation des methodes spécifiques

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\http\Request;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/blog')->name('blog.') -> group(function(){
+    
+Route::get('/', function(Request $request){
+
+
+    return Post::paginate();
+
+    return [
+        "link" => \route('blog.show', ['slug' =>'article', 'id' => 13]),
+    ];
+})->name('index');
+
+
+Route::get('/{slug}-{id}', function ( string $slug, string $id, Request $request){
+    return [
+        'id' => $id,
+        'slug' => $slug,
+        'name' => $request->input('name'),
+    ];
+}) -> where([
+    'id'=> '[0-9]+',
+    'slug' => '[a-zA-Z0-9\-]+'
+])->name('show');
+
 });
